2515a132ae2adbbb9dff060687875916e81a455a
ArtemKezlua <artem.kezlya2001@gmail.com>
2021-10-25 19:00:16 +0300

    error correction

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
-function MyArrayProto() {
-    this.unshift = function (arr) {
-        return arr.unshift('arr')
+class MyArray {
+    constructor(...args) {
+        this.args = args;
     }
-    this.shift = function (arr) {
-        return arr.shift()
+    unshift(el) {
+        const arr = [el]
+        for (let i = 0; i < this.args.length; i++) {
+            arr.push(this.args[i])
+        }
+        this.args = arr
+        return this.args.length
     }
-    this.concat = function (arr1, arr2) {
-        return arr1.concat(arr2)
+    shift() {
+        // let [firstArg,...argsArr] = this.args
+        // this.args = argsArr
+        // return firstArg
+
+        const arr = []
+        let firstElem = this.args[0]
+        for (let i = 1; i < this.args.length; i++) {
+            arr.push(this.args[i])
+        }
+        this.args = arr
+        return firstElem
     }
-    this.reverse = function (arr) {
-        return arr.reverse()
+    concat(...args) {
+        const arr = []
+        for (let i = 0; i < this.args.length; i++) {
+            arr.push(this.args[i])
+        }
+        arr.push(...args)
+        return arr
     }
-    this.map = function (arr) {
-        return arr.map(el => el * 2)
+    reverse() {
+        const arr = []
+        for (let i = this.args.length-1; i >= 0; i--) {
+            arr.push(this.args[i])
+        }
+        return this.args = arr
+    }
+    map(callback) {
+        const arr = []
+        for (let i = 0; i < this.args.length; i++) {
+            arr.push(callback(this.args[i]))
+        }
+        return arr;
     }
 }
 
-let myArrayProto = new MyArrayProto()
-
-myArrayProto.reverse([1,2,3,4])
+const myArray = new MyArray(1,2,3,4)
+console.log(myArray.unshift(0));
+console.log(myArray.shift())
+console.log(myArray.concat(5,6))
+console.log(myArray.reverse())
+console.log(myArray.map(el=>el*2))
